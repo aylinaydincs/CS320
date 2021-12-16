bb44a1a9540b413237a7d3ff22b3df5bab02f4fd
Başak Balcı <bbalci@kandy.io>
2021-12-16 19:19:54 +0300

    Created the Spring Project

---
+++ src/main/java/com/OzUFlix/CS320/OzUFlixApplication.java
@@ -0,0 +1,13 @@
+package com.OzUFlix.CS320;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OzUFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OzUFlixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OzUFlix/CS320/OzUFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OzUFlix.CS320;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OzUFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
