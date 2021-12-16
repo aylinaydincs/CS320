602838061397c20695b3734d7c25577571d3b2f8
Aylin Aydın <aylin.aydin@ozu.edu.tr>
2021-12-16 22:07:59 +0300

    Director model created

---
+++ src/main/java/com/OzUFlix/CS320/Model/Director.java
@@ -0,0 +1,51 @@
+package com.OzUFlix.CS320.Model;
+
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "T_DIRECTOR")
+public class Director {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @NotNull
+    private int id;
+
+    @NotNull
+    @Column(name = "NAME", length = 80)
+    private String name;
+
+    @OneToMany(mappedBy = "director")
+    @NotNull
+    @Column(name = "MOVIE")
+    private List<Movie> movies = new ArrayList<Movie>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+}
