20928ccf67302bd4d56a0b907fe8966cde09c83f
Aylin Aydın <aylin.aydin@ozu.edu.tr>
2021-12-22 22:49:50 +0300

    MovieRepository updated.

--- src/main/java/com/OzUFlix/CS320/Repository/MovieRepository.java
+++ src/main/java/com/OzUFlix/CS320/Repository/MovieRepository.java
@@ -1,12 +1,19 @@
 package com.OzUFlix.CS320.Repository;
 
+import com.OzUFlix.CS320.Model.Available;
 import com.OzUFlix.CS320.Model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
 
     public Movie findById(int id);
+
+    @Query("SELECT m FROM Movie m WHERE m.available = ?1")
+    public List<Movie> searchMovie(int available);
 
 }
