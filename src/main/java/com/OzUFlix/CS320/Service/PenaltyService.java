eaeaffd66363d06619a1ca0a01553943f590c7f4
Aylin Aydın <aylin.aydin@ozu.edu.tr>
2021-12-23 16:39:36 +0300

    PenaltyService findall method and findById method  updated.

--- src/main/java/com/OzUFlix/CS320/Service/PenaltyService.java
+++ src/main/java/com/OzUFlix/CS320/Service/PenaltyService.java
@@ -1,21 +1,37 @@
 package com.OzUFlix.CS320.Service;
 
+import com.OzUFlix.CS320.DTO.DirectorDTO;
+import com.OzUFlix.CS320.DTO.PenaltyDTO;
+import com.OzUFlix.CS320.Model.Director;
 import com.OzUFlix.CS320.Model.Penalty;
+import com.OzUFlix.CS320.Repository.PenaltyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PenaltyService {
     @Autowired
-    PenaltyService penaltyService;
+    PenaltyRepository penaltyRepository;
 
-    public Penalty save(Penalty penalty){ return penaltyService.save(penalty); }
+    public Penalty save(Penalty penalty){ return penaltyRepository.save(penalty); }
 
-    public List<Penalty> findAll(){ return penaltyService.findAll();}
+    public List<PenaltyDTO> findAll(){
+        List<Penalty> penalties = penaltyRepository.findAll();
+        List<PenaltyDTO> penaltyDTOS = new ArrayList<PenaltyDTO>();
+        for (Penalty penalty: penalties) {
+            penaltyDTOS.add(new PenaltyDTO(penalty.getId(),penalty.getUser(),penalty.getRent(),penalty.getReturn_movie()));
+        }
+        return penaltyDTOS;
+    }
 
-    public Penalty findById(int id){ return penaltyService.findById(id); }
+    public PenaltyDTO findById(int id){
+        Penalty penalty = penaltyRepository.findById(id);
+        PenaltyDTO penaltyDTO = new PenaltyDTO(penalty.getId(),penalty.getUser(),penalty.getRent(),penalty.getReturn_movie());
+        return  penaltyDTO;
+    }
 
-    public void deleteById(int id){ penaltyService.deleteById(id); }
+    public void deleteById(int id){ penaltyRepository.deleteById(id); }
 }
