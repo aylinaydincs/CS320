7ec653a28d75518cfce3d81fcb668d100c508565
Aylin Aydın <aylin.aydin@ozu.edu.tr>
2021-12-23 16:43:33 +0300

    MovieService findall method and findById method  updated. MovieService searchMovie method added.

--- src/main/java/com/OzUFlix/CS320/Service/MovieService.java
+++ src/main/java/com/OzUFlix/CS320/Service/MovieService.java
@@ -1,21 +1,58 @@
 package com.OzUFlix.CS320.Service;
 
+import com.OzUFlix.CS320.DTO.DirectorDTO;
+import com.OzUFlix.CS320.DTO.MovieDTO;
+import com.OzUFlix.CS320.Model.Available;
+import com.OzUFlix.CS320.Model.Director;
 import com.OzUFlix.CS320.Model.Movie;
+import com.OzUFlix.CS320.Repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class MovieService {
     @Autowired
-    MovieService movieService;
+    MovieRepository movieRepository;
 
-    public Movie save(Movie movie){ return movieService.save(movie); }
+    public Movie save(Movie movie){ return movieRepository.save(movie); }
 
-    public List<Movie> findAll(){ return movieService.findAll();}
+    public List<MovieDTO> findAll(){
+        List<Movie> movies = movieRepository.findAll();
+        List<MovieDTO> movieDTOs = new ArrayList<MovieDTO>();
+        for (Movie movie: movies) {
+            movieDTOs.add(new MovieDTO(movie.getId(), movie.getName(), movie.getDirector(),movie.getTopic(),movie.getAvailable(),movie.getRents()));
+        }
+        return movieDTOs;
+    }
 
-    public Movie findById(int id){ return movieService.findById(id); }
+    public MovieDTO findById(int id){
+        Movie movie = movieRepository.findById(id);
+        MovieDTO movieDTO = new MovieDTO(movie.getId(), movie.getName(), movie.getDirector(),movie.getTopic(),movie.getAvailable(),movie.getRents());
+        return  movieDTO;
+    }
 
-    public void deleteById(int id){ movieService.deleteById(id); }
+    public List<MovieDTO> searchMovie(String movieInfo){
+        List<MovieDTO> movieDTOS = new ArrayList<>();
+
+        List<Movie> movieNames = movieRepository.findMovieByName(movieInfo);
+        List<Movie> movieTopics = movieRepository.findMovieByTopic(movieInfo);
+        List<Movie> movieDirectors = movieRepository.findMovieByDirector(movieInfo);
+
+        for (Movie movie : movieNames){
+            movieDTOS.add(new MovieDTO(movie.getId(), movie.getName(), movie.getDirector(),movie.getTopic(),movie.getAvailable(),movie.getRents()));
+        }
+        for (Movie movie : movieTopics){
+            movieDTOS.add(new MovieDTO(movie.getId(), movie.getName(), movie.getDirector(),movie.getTopic(),movie.getAvailable(),movie.getRents()));
+        }
+        for (Movie movie : movieDirectors){
+            movieDTOS.add(new MovieDTO(movie.getId(), movie.getName(), movie.getDirector(),movie.getTopic(),movie.getAvailable(),movie.getRents()));
+        }
+
+        return movieDTOS;
+    }
+
+    public void deleteById(int id){ movieRepository.deleteById(id); }
 }
