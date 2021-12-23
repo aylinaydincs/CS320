a0d57fe2ab45055fe0049afcbc217b1b05bc04a9
Aylin Aydın <aylin.aydin@ozu.edu.tr>
2021-12-23 15:37:26 +0300

    Test file created and services added.

--- src/test/java/com/OzUFlix/CS320/OzUFlixApplicationTests.java
+++ src/test/java/com/OzUFlix/CS320/OzUFlixApplicationTests.java
@@ -1,13 +1,35 @@
 package com.OzUFlix.CS320;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import com.OzUFlix.CS320.Model.*;
+import com.OzUFlix.CS320.Service.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OzUFlixApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	AvailableService availableService;
 
-}
+	@Autowired
+	DirectorService directorService;
+
+	@Autowired
+	TopicService topicService;
+
+	@Autowired
+	MovieService movieService;
+
+	@Autowired
+	UserService userService;
+
+	@Autowired
+	RentService rentService;
+
+	@Autowired
+	Return_MovieService return_movieService;
+
+	@Autowired
+	PenaltyService penaltyService;}
