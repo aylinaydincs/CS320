eab02391da080f0d2a9ccf9ca14deb66c2056c76
Yigit Cicioglu <yigit.cicioglu@ozu.edu.tr>
2021-12-26 17:26:24 +0300

    Repository updated

--- src/main/java/com/OzUFlix/CS320/Repository/AvailableRepository.java
+++ src/main/java/com/OzUFlix/CS320/Repository/AvailableRepository.java
@@ -1,12 +1,17 @@
 package com.OzUFlix.CS320.Repository;
 
 import com.OzUFlix.CS320.Model.Available;
+import com.OzUFlix.CS320.Model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AvailableRepository extends JpaRepository<Available, Integer> {
 
     public Available findById(int id);
+
 
 }
--- src/main/java/com/OzUFlix/CS320/Repository/UserRepository.java
+++ src/main/java/com/OzUFlix/CS320/Repository/UserRepository.java
@@ -1,12 +1,25 @@
 package com.OzUFlix.CS320.Repository;
 
+import com.OzUFlix.CS320.Model.Rent;
 import com.OzUFlix.CS320.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     public User findById(int id);
+
+    @Query("SELECT u FROM User AS u WHERE u.name=?1 and u.password=?2")
+    public User findUserByNameAndPassword(String name, String password);
+
+    @Query("SELECT u FROM User AS u WHERE u.id=?1 and u.userType=1")
+    public User getUserByUsertype(int id);
+
+    @Query("SELECT r FROM Rent AS r inner join r.user as u WHERE u.id=?1")
+    public List<Rent> getRent(int userId);
 
 }
